dd91620105396dee45d5b4d786be48b1a68e810f
cbj <cqu45a@naver.com>
2022-06-11 11:45:27 +0900

    1111

--- app/src/main/java/com/example/test/MainPageActivity.java
+++ app/src/main/java/com/example/test/MainPageActivity.java
@@ -1,35 +1,50 @@
 package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
-
-import java.util.ArrayList;
+import android.widget.TextView;
 
 public class MainPageActivity extends AppCompatActivity {
 
     FrameLayout flMenu, flPicture, flText;
-    ImageButton imgbtnHead;
+    ImageButton imgbtnHead, imgbtnOther, imgbtnBok, imgbtnGol, imgbtnBody, imgbtnMok, imgbtnArm;
     ImageButton imgbtnMenu, imgbtnAid;
     Integer count = 0;
+
+    public static Context context; //다른 액티비티에 변수 전달
+    public static String body_num; //부위별 검색 코드
+    TextView body_tv; //버튼클릭시 적용되는 부위 확인용으로 나중에 지워도됨
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
 
-        imgbtnHead = findViewById(R.id.imgbtn_head);
+        context=this;
+
+        imgbtnArm = findViewById(R.id.btArm);
+        imgbtnHead = findViewById(R.id.btHead);
+        imgbtnOther = findViewById(R.id.btOther);
+        imgbtnBok = findViewById(R.id.btBok);
+        imgbtnGol = findViewById(R.id.btGol);
+        imgbtnBody = findViewById(R.id.btBody);
+        imgbtnMok = findViewById(R.id.btMok);
+
+
         imgbtnMenu = findViewById(R.id.imgbtn_menu);
         imgbtnAid = findViewById(R.id.imgBtn_aid);
         flMenu = (FrameLayout) findViewById(R.id.FL_Menu) ;
         flPicture = (FrameLayout) findViewById(R.id.FL_Picture);
         flText = (FrameLayout) findViewById(R.id.FL_Text);
+
+        body_tv = (TextView) findViewById(R.id.bodyTv);
 
         imgbtnAid.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +53,55 @@ public class MainPageActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+        //팔 버튼 클릭
+        imgbtnArm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                body_tv.setText("외과");
+                body_num = "04";//해당하는 부위의 검색코드
+            }
+        });
+        //기타
+        imgbtnOther.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                body_tv.setText("응급의학과");
+                body_num = "24";//해당하는 부위의 검색코드
+            }
+        });
+        //복부
+        imgbtnBok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                body_tv.setText("내과");
+                body_num = "01";//해당하는 부위의 검색코드
+            }
+        });
+        //골반
+        imgbtnGol.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                body_tv.setText("비뇨의학과");
+                body_num = "15";//해당하는 부위의 검색코드
+            }
+        });
+        //전신
+        imgbtnBody.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                body_tv.setText("한방내과");
+                body_num = "80";//해당하는 부위의 검색코드
+            }
+        });
+        //목
+        imgbtnMok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                body_tv.setText("흉부외과");
+                body_num = "07";//해당하는 부위의 검색코드
+            }
+        });
+        //머리
         imgbtnHead.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -62,6 +125,16 @@ public class MainPageActivity extends AppCompatActivity {
                     flText.setVisibility(View.VISIBLE);
                     count = 0;
                 }
+            }
+        });
+
+        //검색버튼으로 클릭시 메인액티비티로 전환
+        Button bt_search = (Button) findViewById(R.id.btSearch);
+        bt_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ParsingActivity.class);
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/test/detailpageActivity.java
+++ app/src/main/java/com/example/test/detailpageActivity.java
@@ -2,27 +2,86 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class detailpageActivity extends AppCompatActivity {
 
     ImageButton imgbtnBack;
+    ImageButton imgbtnDetail_Head, imgbtnDetail_Eyes, imgbtnDetail_LeftEar, imgbtnDetail_RightEar, imgbtnDetail_Nose, imgbtnDetail_Mouth;
+    TextView tvDetail;
+
+    public static Context context; //다른 액티비티에 변수 전달
+    public static String body_num; //부위별 검색 코드
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailpage);
 
+        context=this;
+
+        tvDetail = findViewById(R.id.Detail_Result);
+
         imgbtnBack = findViewById(R.id.imgbtn_Back);
+        imgbtnDetail_Head = findViewById(R.id.btDetail_Head);
+        imgbtnDetail_Eyes = findViewById(R.id.btDetail_Eyes);
+        imgbtnDetail_LeftEar = findViewById(R.id.btDetail_LeftEar);
+        imgbtnDetail_RightEar = findViewById(R.id.btDetail_RightEar);
+        imgbtnDetail_Nose = findViewById(R.id.btDetail_Nose);
+        imgbtnDetail_Mouth = findViewById(R.id.btDetail_Mouth);
 
         imgbtnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(detailpageActivity.this, MainPageActivity.class);
                 startActivity(intent);
+            }
+        });
+        imgbtnDetail_Head.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDetail.setText("정신건강의학과");
+                body_num = "03";//해당하는 부위의 검색코드
+            }
+        });
+        imgbtnDetail_Eyes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDetail.setText("안과");
+                body_num = "12";//해당하는 부위의 검색코드
+            }
+        });
+        imgbtnDetail_LeftEar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDetail.setText("이비인후과");
+                body_num = "13";//해당하는 부위의 검색코드
+            }
+        });
+        imgbtnDetail_RightEar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDetail.setText("이비인후과");
+                body_num = "13";//해당하는 부위의 검색코드
+            }
+        });
+        imgbtnDetail_Nose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDetail.setText("이비인후과");
+                body_num = "13";//해당하는 부위의 검색코드
+            }
+        });
+        imgbtnDetail_Mouth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvDetail.setText("통합치의학과");
+                body_num = "61";//해당하는 부위의 검색코드
             }
         });
     }
